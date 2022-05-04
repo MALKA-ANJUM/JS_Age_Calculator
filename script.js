bd4a2c0d6cf166606b9b10ccc44c4b71e659cfc2
Malka Anjum <75417185+MALKA-ANJUM@users.noreply.github.com>
2022-05-04 17:42:36 +0530

    main commit

--- script.js
+++ script.js
@@ -2,52 +2,52 @@ console.log("hello");
 
 // Variables For Datepicker
 
-let datePicker = document.getElementById('datePicker');
-let choseDate = document.getElementById('choseDate');
+var datePicker = document.getElementById('datePicker');
+var choseDate = document.getElementById('choseDate');
 
 // Variables For Prinitng Values
 
-let ageYear = document.getElementById('ageYear');
-let ageMonth = document.getElementById('ageMonth');
-let ageDays = document.getElementById('ageDays');
-let ageHours = document.getElementById('ageHours');
-let ageSeconds = document.getElementById('ageSeconds');
-let ageMiliSeconds = document.getElementById('ageMiliSeconds');
+var ageYear = document.getElementById('ageYear');
+var ageMonth = document.getElementById('ageMonth');
+var ageDays = document.getElementById('ageDays');
+var ageHours = document.getElementById('ageHours');
+var ageSeconds = document.getElementById('ageSeconds');
+var ageMiliSeconds = document.getElementById('ageMiliSeconds');
 
 datePicker.addEventListener('change',function(){
 
-	let options = {year: 'numeric', month: 'long', day: 'numeric'};
-	let selectedDate = new Date(this.value);
-	let DOB = selectedDate.toLocaleDateString('en-US',options);
+	var options = {year: 'numeric', month: 'long', day: 'numeric'};
+	var selectedDate = new Date(this.value);
+	var DOB = selectedDate.toLocaleDateString('en-US',options);
 
 	
 	choseDate.innerHTML = "DOB : " + " " + DOB;
 
 
-	let miliSeconds_Btw_DOB = Date.parse(DOB);
-	let miliSecond_Btw_Now = Date.now();
+	var miliSeconds_Btw_DOB = Date.parse(DOB);
+	var miliSecond_Btw_Now = Date.now();
 
-	let age_in_MiliSeconds = miliSecond_Btw_Now -  miliSeconds_Btw_DOB;
+	var age_in_MiliSeconds = miliSecond_Btw_Now -  miliSeconds_Btw_DOB;
 
 	// console.log(age_in_MiliSeconds);
 
-	let miliSeconds = age_in_MiliSeconds;
-	let second = 1000;
-	let minute = second*60;
-	let hour = minute*60;
-	let day = hour*24;
-	let month = day*30;
-	let year = day*365;
+	var miliSeconds = age_in_MiliSeconds;
+	var second = 1000;
+	var minute = second*60;
+	var hour = minute*60;
+	var day = hour*24;
+	var month = day*30;
+	var year = day*365;
 
 
 
 	// NOW START THE CALCULATION
 
-	let years = Math.round(miliSeconds/year);
-	let months = years*12;
-	let days = years*365;
-	let hours = Math.round(miliSeconds/hour);
-	let seconds = Math.round(miliSeconds/second);
+	var years = Math.round(miliSeconds/year);
+	var months = years*12;
+	var days = years*365;
+	var hours = Math.round(miliSeconds/hour);
+	var seconds = Math.round(miliSeconds/second);
 
 
 
